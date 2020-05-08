735b749eadf856d3c4939ae30a0f43712ed02b50
PC <PC@10.192.1.61>
2020-05-08 19:22:50 +0200

    Ajout de la partir item et test sur un jeu de donné réduit

---
+++ src/app/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UserService {
+  private _url:string="data.json";
+  constructor (private _http: Http) {}
+
+  getUsers() {
+    return this._http.get(this._url).map((res:Response) => res.json());
+  }
+
+}
