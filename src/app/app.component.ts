7bb51b16e8ea96d3f590632c8308755cedc945d5
PC <PC@10.192.1.61>
2020-05-08 18:52:24 +0200

    Création de service.ts, modification du code pour pouvoir afficher du Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
+import {Component, OnInit, Injectable} from '@angular/core';
+import { Http, Headers, Response  } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { Observable, Subject} from 'rxjs/Rx';
+import  {UserService} from './user.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';    
+import 'rxjs/add/operator/toPromise';  
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -24,6 +31,17 @@ export class AppComponent {
 
 @Injectable()
 
-export class ConfigService {
-  constructor(private http: HttpClient) { }
+export class ServiceComponent implements OnInit {
+
+  civil=[];
+
+  constructor(private _userService : UserService) {  }
+
+  ngOnInit() {
+    //this.data = this._userService.getUsers();
+    this._userService.getUsers().subscribe(resUserData => this.civil = resUserData);
+  }
+
+  headers = new Headers({'Content-Type': 'application/json','Accept': 'application/json'});
+
 }
