6ead76587d2de1db3fb296a7696e3cfab3f270f6
JB <collonguesj97@gmail.com>
2020-05-08 18:33:41 +0200

    ajout requet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-web-application';
+  readonly ROOT_URL='https://data.rennesmetropole.fr/explore/dataset/actes-detat-civil/';
+
+  post: Observable<any>;
+
+  constructor(private http: HttpClient){}
+
+  getPost(){
+    this.post = this.http.get(this.ROOT_URL)
+  }
+}
+
+@Injectable()
+
+export class ConfigService {
+  constructor(private http: HttpClient) { }
 }
