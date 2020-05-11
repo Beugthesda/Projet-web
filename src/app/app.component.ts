04fb2616a4b95f2e189318464de0d86dadabf367
PC <PC@10.192.1.61>
2020-05-11 18:26:19 +0200

    Retour version fonctionelle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,4 @@
-import {Component, OnInit, Injectable} from '@angular/core';
-import { Http, Headers, Response  } from '@angular/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { Observable, Subject} from 'rxjs/Rx';
-import  {UserService} from './user.service';
-import 'rxjs/add/operator/map';
-import 'rxjs/Rx';    
-import 'rxjs/add/operator/toPromise';  
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -18,30 +7,4 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'projet-web-application';
-  readonly ROOT_URL='https://data.rennesmetropole.fr/explore/dataset/actes-detat-civil/';
-
-  post: Observable<any>;
-
-  constructor(private http: HttpClient){}
-
-  getPost(){
-    this.post = this.http.get(this.ROOT_URL)
-  }
-}
-
-@Injectable()
-
-export class ServiceComponent implements OnInit {
-
-  civil=[];
-
-  constructor(private _userService : UserService) {  }
-
-  ngOnInit() {
-    //this.data = this._userService.getUsers();
-    this._userService.getUsers().subscribe(resUserData => this.civil = resUserData);
-  }
-
-  headers = new Headers({'Content-Type': 'application/json','Accept': 'application/json'});
-
 }
